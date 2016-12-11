1223b74d55ed8590fb96f2b0f63715b4afbc6cd9
Sergey Seroukhov <sseroukhov@azure.skynet.network>
2016-12-11 04:36:30 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Data processing and persistence components for Pip.Services in .NET Changelog
+
+## <a name="1.0.1-1.0.4"></a> 1.0.1-1.0.4 (2016-12-09)
+
+### Features
+* **mongodb** Implemented consistent connection and credential parameters
+
+### Bug Fixes
+* Added description to NuGet package
+
+## <a name="1.0.0"></a> 1.0.0 (2016-11-21)
+
+Initial public release
+
+### Features
+* **log** Memory persistence
+* **file** Abstract file and JSON persistence
+* **mongodb** MongoDB persistence
+
+### Breaking Changes
+No breaking changes since this is the first version
+
+### Bug Fixes
+No fixes in this version
+
---
+++ src/PipServices.Azure.Net45/Count/AppInsightsCounters.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using PipServices.Commons.Auth;
+using PipServices.Commons.Config;
+using PipServices.Commons.Count;
+using PipServices.Commons.Refer;
+using System.Collections.Generic;
+
+namespace PipServices.Azure.Count
+{
+    public class AppInsightsCounters : CachedCounters, IDescriptable
+    {
+        private string _name;
+        private CredentialResolver _credentialResolver = new CredentialResolver();
+        private TelemetryClient _client;
+
+        public AppInsightsCounters() { }
+
+        public Descriptor GetDescriptor()
+        {
+            var name = _name ?? "default";
+            return new Descriptor("pip-services-azure", "counters", "app-insights", name, "1.0");
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public override void Configure(ConfigParams config)
+        {
+            base.Configure(config);
+            _credentialResolver.Configure(config, true);
+            _name = NameResolver.Resolve(config, _name);
+        }
+
+        private void Open()
+        {
+            var credential = _credentialResolver.LookupAsync("count").Result;
+
+            var key = credential.AccessKey 
+                ?? credential.GetAsNullableString("instrumentation_key")
+                 ?? credential.GetAsNullableString("InstrumentationKey");
+
+            if (key != null)
+                TelemetryConfiguration.Active.InstrumentationKey = key;
+
+            _client = new TelemetryClient();
+        }
+
+        protected override void Save(IEnumerable<Counter> counters)
+        {
+            if (_client == null) Open();
+
+            foreach (var counter in counters)
+            {
+                switch (counter.Type)
+                {
+                    case CounterType.Increment:
+                        _client.TrackMetric(counter.Name, counter.Count.Value);
+                        break;
+                    case CounterType.Interval:
+                        _client.TrackMetric(counter.Name, counter.Average.Value);
+                        break;
+                    case CounterType.LastValue:
+                        _client.TrackMetric(counter.Name, counter.Last.Value);
+                        break;
+                    case CounterType.Statistics:
+                        _client.TrackMetric(counter.Name, counter.Average.Value);
+                        break;
+                }
+            }
+
+            _client.Flush();
+        }
+    }
+}
