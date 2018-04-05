dbc25e608492a276f59f4a7503de3794d2980cfe
Alexander Mazur <alexander.mazur@hotmail.com>
2018-04-05 11:25:56 -0700

    Add DefaultAzureFactory and AppInsightsLogger

---
+++ src/Build/DefaultAzureFactory.cs
@@ -0,0 +1,31 @@
+﻿using PipServices.Commons.Build;
+using PipServices.Commons.Refer;
+using PipServices.Azure.Messaging;
+using PipServices.Azure.Config;
+using PipServices.Azure.Count;
+using PipServices.Azure.Log;
+
+namespace PipServices.Azure.Build
+{
+    public class DefaultAzureFactory : Factory
+    {
+        public static Descriptor Descriptor = new Descriptor("pip-services-azure", "factory", "default", "default", "1.0");
+
+        public static Descriptor StorageMessageQueueDescriptor = new Descriptor("pip-services-azure", "messaging", "storage-message-queue", "default", "1.0");
+        public static Descriptor ServiceBusMessageQueueDescriptor = new Descriptor("pip-services-azure", "messaging", "servicebus-message-queue", "*", "1.0");
+        public static Descriptor ServiceBusMessageTopicDescriptor = new Descriptor("pip-services-azure", "messaging", "servicebus-message-topic", "*", "1.0");
+        public static Descriptor KeyVaultConfigReaderDescriptor = new Descriptor("pip-services-azure", "config-reader", "key-vault", "*", "1.0");
+        public static Descriptor AppInsightsCountersDescriptor = new Descriptor("pip-services-azure", "counters", "app-insights", "*", "1.0");
+        public static Descriptor AppInsightsLoggerDescriptor = new Descriptor("pip-services-azure", "logger", "app-insights", "*", "1.0");
+
+        public DefaultAzureFactory()
+        {
+            RegisterAsType(StorageMessageQueueDescriptor, typeof(StorageMessageQueue));
+            RegisterAsType(ServiceBusMessageQueueDescriptor, typeof(ServiceBusMessageQueue));
+            RegisterAsType(ServiceBusMessageTopicDescriptor, typeof(ServiceBusMessageTopic));
+            RegisterAsType(KeyVaultConfigReaderDescriptor, typeof(KeyVaultConfigReader));
+            RegisterAsType(AppInsightsCountersDescriptor, typeof(AppInsightsCounters));
+            RegisterAsType(AppInsightsLoggerDescriptor, typeof(AppInsightsLogger));
+        }
+    }
+}
